e9db10cdd4a36123b173e7fd7afc696bb1a22d02
Kevin <gitOnRaspberryPi@haz-zone.com>
2013-07-31 08:38:24 -0400

    adding home-use tool to github

---
+++ printKeys.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Use as:
+# from printKeys import printKeys
+
+# Recursively prints ".keys()" for objects mixed with dictionaries, lists, 
+# integers, longs, floats, or strings. 
+def printKeys(mixed_object, spacing = 0):
+  tab = ""
+  for i in range(spacing):
+    tab += " "
+  for k in mixed_object: #sorted(mixed_object, key=mixed_object.get):
+    if type(mixed_object[k]) == dict:
+      print tab + k
+      printKeys(mixed_object[k], spacing + 2)
+    elif type(mixed_object[k]) == list:
+      for l in range(len(mixed_object[k])):
+        print tab + k
+        printKeys(mixed_object[k][l], spacing + 2)
+    elif type(mixed_object[k]) in [int, long, float]:
+      print tab + str(k)
+    else:
+      print tab + k
